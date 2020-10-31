3d4513e58ecf415340d77647c209051eace1a714
JulianRubinstein <70553224+JulianRubinstein@users.noreply.github.com>
2020-10-31 03:25:37 -0700

    Update tiktok.py

--- tiktok.py
+++ tiktok.py
@@ -3,29 +3,24 @@ from TikTokApi.browser import set_async
 import time
 import matplotlib.pyplot as plt
 from scipy import stats
-from flask import Flask, jsonify
-
-# RUN THIS PART ONLY WHEN FIRST RUNNNING THE PROGRAM
-
-########################################## Acquiring the videos ############################################
+from flask import Flask, request
+from flask_restful import Api, Resource
 
 #Setting the API
 set_async()
-api = TikTokApi()
+tiktokapi = TikTokApi()
 
 #Acquiring the videos and arranging the view numbers in a list (reversing as the original order is newer first)
-def extract_data(**kwargs):
-    videos = api.byUsername(username=user, count=results)
+def extract_data(tiktokapi, user, videos_num):
+    videos = tiktokapi.byUsername(username=user, count=videos_num)
     video_views = list([video['stats']['playCount'] for video in videos])
     video_times = list([video['createTime'] for video in videos])
     video_views.reverse()
     video_times.reverse()
     return [video_views, video_times]
 
-########################################### Fitting the model ###############################################
-
-#Defining a function to reject outliers and videos from last two days
-def reject_outliers(**kwargs):
+#Reject outlier videos and videos from last two days
+def reject_outliers(video_views, video_times, outlier_sensitivity, videos_for_virality, num_days):
 
     #Internal variables
     current_time = time.time()
@@ -48,35 +43,42 @@ def reject_outliers(**kwargs):
     
     return [x_filtered,video_views_filtered, odds_of_virality]
 
-def fit_model(**kwargs):
+#Fitting the model to filtered data
+def fit_model(x_filtered, video_views_filtered, video_views):
 
-    #Fitting the model to filtered data
     x=[i for i in range(len(video_views))]
     slope, intercept, r_value, p_value, std_err = stats.linregress(x_filtered, video_views_filtered)
     next_video_views = int(slope*(len(video_views)) + intercept)
     
-    #Bulding figure for inner use
-    plt.plot(x, [slope*k + intercept for k in x], label = "Fitted model")
-    plt.scatter(len(video_views),slope*(len(video_views)) + intercept, label = "Next video prediction")
-    plt.scatter(x,video_views, label='Ignored data')
-    plt.scatter(x_filtered,video_views_filtered, label='Fitted data')
-    plt.title("Views vs Video: " + user)
-    plt.xlabel('Video')
-    plt.ylabel('Number of views')
-    plt.legend()    
-    
-    #Printing data for inner use
-    print("The odds to go viral are " + str(int(odds_of_virality*100)) + "%")
-    if (next_video_views>0):
-        print("The predicted views for the next video are " + str(next_video_views))
-    else:
-        print("The predicted views for the next video are below zero")
-    print("the p value is: " + str(p_value) + " (above 0.05 is reccomended)")
-    print(std_err)
-    
     return [slope, intercept, p_value, next_video_views, x] 
 
-def create_data(**kwargs):  
+#Printing data for internal use
+def print_model(x, slope, intercept, video_views, x_filtered, video_views_filtered, user, odds_of_virality, next_video_views, p_value, result, outlier_sensitivity):
+   
+     #Bulding figure
+     plt.figure()
+     plt.plot(x, [slope*k + intercept for k in x], label = "Fitted model")     
+     plt.scatter(len(video_views),slope*(len(video_views)) + intercept, label = "Next video prediction")
+     plt.scatter(x,video_views, label='Ignored data')
+     plt.scatter(x_filtered,video_views_filtered, label='Fitted data')
+     plt.title("Views vs Video: " + user)
+     plt.xlabel('Video')
+     plt.ylabel('Number of views')
+     plt.legend()    
+   
+     #Printing data
+     print("The odds to go viral are " + str(int(odds_of_virality*100)) + "%")
+     if (next_video_views>0):
+         print("The predicted views for the next video are " + str(next_video_views))
+     else:
+         print("The predicted views for the next video are below zero")
+     print("The p value is: " + str(p_value) + " (above 0.05 is reccomended)")
+     print("The number videos chosen: " + str(result))
+     print(" The outlier sensitivity of: " + str(outlier_sensitivity))
+     return None
+
+#Create a dictionary to send to the api request
+def create_data(x, video_views, x_filtered, video_views_filtered, slope, intercept, p_value, next_video_views, odds_of_virality, outlier_sensitivity):  
     data = {"x":x,
             "video_views":video_views,
             "x_filtered":x_filtered, 
@@ -85,59 +87,72 @@ def create_data(**kwargs):
             "slope":slope, 
             "intercept":intercept, 
             "p_value":p_value, 
-            "next_video_views":next_video_views
+            "next_video_views":next_video_views,
+            "outlier_senitivity":outlier_sensitivity
             }
     return data
 
-#Global variables for later use
-prev_1=0
-prev_2=0
-
-########################################## USER INPUT ##################################################
-#%% RUN THIS PART WHEN CHANGING INPUT
-
-#USER INPUT VARIABLES
-results = 10 #Number of videos wanted
-user="ecruzn" #TikTok username
-outlier_sensitivity = 0.1 #From 0.1 to 0.5
-videos_for_virality = 10 #Number of videos to check for virality
-num_days = 2 #Num of days to ignore
-
-
-#This if statement checks wether the number of results or username has changed to avoid extracting data for no reason
-if (prev_1!=results or prev_2!=user):
-    video_views, video_times = extract_data(api = api, user = user, results = results)
-x_filtered, video_views_filtered, odds_of_virality = reject_outliers(video_views = video_views, video_times = video_times, outlier_sensitivity = outlier_sensitivity, videos_for_virality = videos_for_virality, num_days = num_days)
-slope, intercept, p_value, next_video_views, x = fit_model(x_filtered=x_filtered, video_views_filtered = video_views_filtered)
-        
-###################################### NEW IMPLEMENTATION STILL IN PROGRESS ##############################
-# sensitivities = [0.1*i for i in range(1,11)]
-# result_list = [10*i for i in range(1,5)]
-
-# p_values=[]
-
-# for result in result_list:
-#     for outlier_sensitivity in sensitivities:
-#         video_views = video_views[-result:]
-#         video_times = video_times[-result:]
-#         x_filtered, video_views_filtered, odds_of_virality = reject_outliers(video_views = video_views, video_times = video_times, outlier_sensitivity = outlier_sensitivity, videos_for_virality = videos_for_virality, num_days = num_days)
-#         slope, intercept, p_value, next_video_views, x = fit_model(x_filtered=x_filtered, video_views_filtered = video_views_filtered)
-#         p_values.append((p_value, result, outlier_sensitivity))
-        
-####################################################################################################
-
-prev_1 = results
-prev_2 = user
-
-#%%
-############################ Creating a data object and a flask endpoint ##################################
+#The main function which calls all other functions
+def model(user):
+    
+    #Default variables
+    videos_num = 100 #Number of videos wanted
+    videos_for_virality = 10 #Number of videos to check for virality
+    num_days = 2 #Num of days to ignore
+    
+    #Defining list of various sensitivies and number of videos to check back.
+    sensitivities = [0.1*i for i in range(1,11)]
+    result_list = [10*i for i in range(1,11)]
+    p_values=[]
+    values=[]
+    
+    #Extracting the data using the tiktokapi
+    video_views, video_times = extract_data(tiktokapi, user, videos_num)
+    
+    #Checking the p_value for various video numbers and sensitivities
+    for result in result_list:
+        for outlier_sensitivity in sensitivities:
+            video_views = video_views[-result:]
+            video_times = video_times[-result:]
+            x_filtered, video_views_filtered, odds_of_virality = reject_outliers(video_views, video_times, outlier_sensitivity, videos_for_virality, num_days)
+            slope, intercept, p_value, next_video_views, x = fit_model(x_filtered, video_views_filtered, video_views)
+            values.append((result, outlier_sensitivity))
+            p_values.append(p_value)
+    
+    #Choosing the video with best p_value
+    result, outlier_sensitivity = values[p_values.index(max(p_values))]
+    
+    #Using the relevant part of the data according to above p_value (without re-extracting the data for perfomance purposes)
+    video_views = video_views[-result:]
+    video_times = video_times[-result:]
+    
+    #Refiting the data with the best p_value and creating the data that will be sent to the client
+    x_filtered, video_views_filtered, odds_of_virality = reject_outliers(video_views, video_times, outlier_sensitivity, videos_for_virality, num_days)
+    slope, intercept, p_value, next_video_views, x = fit_model(x_filtered, video_views_filtered, video_views)
+    data = create_data(x, video_views, x_filtered, video_views_filtered, slope, intercept, p_value, next_video_views, odds_of_virality, outlier_sensitivity)
+    
+    #Printing data for internal use
+    print_model(x, slope, intercept, video_views, x_filtered, video_views_filtered, user, odds_of_virality, next_video_views, p_value, result, outlier_sensitivity)
+    
+    #Returning the data
+    return data
 
 app = Flask(__name__)
+api = Api(app)
 
-@app.route('/', methods=['GET'])
+class Data(Resource):
+   def post(self):
+       #Requesting a user from the client and returning the dataset
+       postedData = request.get_json()
+       user = postedData["user"]
+       data = model(user)
+       return data
+
+api.add_resource(Data, "/getdata")
+
+@app.route('/')
 def home():
-    data = create_data(x = x, video_views = video_views, x_filtered = x_filtered, video_views_filtered = video_views_filtered, slope = slope, intercept = intercept, p_value = p_value, next_video_views = next_video_views)
-    return jsonify(data)
+    return "Test"
 
 if __name__=="__main__":
     app.run()
